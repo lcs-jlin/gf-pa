7e7d348225a8c0c1b6a4210606642dee936fce31
Justy Lin <jlin@lcs.on.ca>
2019-04-04 14:31:20 -0400

    Fixed class names

---
+++ MyGame/Background.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class background here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Background extends World
+{
+
+    /**
+     * Constructor for objects of class background.
+     * 
+     */
+    public Background()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 600, 1); 
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+    }
+}
---
+++ MyGame/Lemur.java
@@ -0,0 +1,32 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class lemur here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lemur extends Actor
+{
+    /**
+     * Act - do whatever the lemur wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if (isAtEdge())
+        {
+            turn(17);
+        }
+        if (Greenfoot.getRandomNumber(100)<10)
+        {
+            turn(Greenfoot.getRandomNumber(90)-45);
+        }
+        move(8);
+        if ( isTouching(Sheep.class) );
+        {
+            removeTouching(Sheep.class);
+        }
+    }
+
+}
